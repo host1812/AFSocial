f00dd04d3f6590ee41670b5eaf2440421038b0c7
host1812 <host1812@gmail.com>
2025-01-27 08:44:05 -0500

    add interactiontype

---
+++ sources/AFSocial.Domain/Aggregates/PostAggregate/InteractionType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFSocial.Domain.Aggregates.PostAggregate;
+public enum InteractionType
+{
+    Like,
+    Dislike,
+    Haha,
+    Wow,
+    Love,
+    Angry
+}
