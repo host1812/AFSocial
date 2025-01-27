d5753023644a09008c66e6a8d2873e476ee90528
host1812 <host1812@gmail.com>
2025-01-27 09:44:48 -0500

    before migration

---
+++ sources/AFSocial.Api/Registars/DbRegistrar.cs
@@ -0,0 +1,13 @@
+﻿
+using AFSocial.Data;
+
+namespace AFSocial.Api.Registars;
+
+public class DbRegistrar : IWebApplicationBuilderRegistar
+{
+    public void RegisterServices(WebApplicationBuilder builder)
+    {
+        var cs = builder.Configuration.GetConnectionString("Default");
+        builder.Services.AddDbContext<DataContext>();
+    }
+}
--- sources/AFSocial.Data/DataContext.cs
+++ sources/AFSocial.Data/DataContext.cs
@@ -1,8 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AFSocial.Domain.Aggregates.PostAggregate;
+using AFSocial.Domain.Aggregates.UserProfileAggregate;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace AFSocial.Data;
 
-public class DataContext : IdentityDbContext
+public class DataContext(DbContextOptions options) : IdentityDbContext(options)
 {
+    public DbSet<UserProfile> UserProfiles { get; set; }
+    public DbSet<Post> Posts { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
 }
