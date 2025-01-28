f8865907a3276314a686dae8ff988aac6cd2e1db
host1812 <host1812@gmail.com>
2025-01-28 17:58:52 -0500

    user profiles contoller

---
+++ sources/AFSocial.Api/Controllers/v1/UserProfilesController.cs
@@ -0,0 +1,26 @@
+﻿using AFSocial.Domain.Aggregates.PostAggregate;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AFSocial.Api.Controllers.v1;
+
+[ApiController]
+[ApiVersion("1")]
+[Route(ApiRoutes.BaseDefaultRoute)]
+[Route(ApiRoutes.BaseVersionedRoute)]
+public class UserProfilesController : ControllerBase
+{
+    public UserProfilesController()
+    {
+    }
+
+    public async Task<IActionResult> GetAllProfiles()
+    {
+        return Ok();
+    }
+
+    public async Task<IActionResult> CreateUserProfile()
+    {
+        return Ok();
+    }
+}
