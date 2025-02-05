451e5aeee6ae1ba2e1f09d998d79d82af8619302
host1812 <host1812@gmail.com>
2025-02-05 08:50:57 -0500

    add basic info validator

---
+++ sources/AFSocial.Domain/Validators/UserProfileValidators/BasicInfoValidator.cs
@@ -0,0 +1,37 @@
+﻿using AFSocial.Domain.Aggregates.UserProfileAggregate;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFSocial.Domain.Validators.UserProfileValidators;
+public class BasicInfoValidator : AbstractValidator<BasicInfo>
+{
+    public BasicInfoValidator()
+    {
+        RuleFor(profile => profile.FirstName)
+            .NotNull().WithMessage("First name is required.")
+            .MinimumLength(3).WithMessage("First name length should be more then 3 chars.")
+            .MaximumLength(50).WithMessage("First name length should be less then 50 chars.");
+
+        RuleFor(profile => profile.LastName)
+            .NotNull().WithMessage("Last name is required.")
+            .MinimumLength(3).WithMessage("Last name length should be more then 3 chars.")
+            .MaximumLength(50).WithMessage("Last name length should be less then 50 chars.");
+
+        RuleFor(profile => profile.EmailAddress)
+            .NotNull().WithMessage("Email address is required.")
+            .EmailAddress().WithMessage("Email address should be a valid email address.");
+
+        RuleFor(profile => profile.DateOfBirth)
+            .NotNull().WithMessage("Date of birth is required.")
+            .InclusiveBetween(
+                new DateTime(DateTime.Now.AddYears(-125).Ticks),
+                new DateTime(DateTime.Now.AddYears(-15).Ticks))
+            .WithMessage($"Date of birth should be between "
+                         + $"{new DateTime(DateTime.Now.AddYears(-125).Ticks)}"
+                         + $"and {new DateTime(DateTime.Now.AddYears(-15).Ticks)}.");
+    }
+}
