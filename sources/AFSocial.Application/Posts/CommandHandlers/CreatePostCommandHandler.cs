c90615e8da417f6595d7fe7624654e4c5f9b59e7
host1812 <host1812@gmail.com>
2025-02-11 08:39:00 -0500

    add validation error handling

--- sources/AFSocial.Application/Posts/CommandHandlers/CreatePostCommandHandler.cs
+++ sources/AFSocial.Application/Posts/CommandHandlers/CreatePostCommandHandler.cs
@@ -2,8 +2,10 @@
 using AFSocial.Application.Posts.Commands;
 using AFSocial.Data;
 using AFSocial.Domain.Aggregates.PostAggregate;
+using AFSocial.Domain.Exceptions;
 using Azure.Core;
 using MediatR;
+using Microsoft.EntityFrameworkCore.Update;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,6 +32,20 @@ public class CreatePostCommandHandler :
             ctx.Posts.Add(post);
             await ctx.SaveChangesAsync();
             result.Value = post;
+        }
+        catch (PostNotValidException ex)
+        {
+            result.IsError = true;
+            ex.ValidationErrors.ForEach(e =>
+            {
+                var error = new OperationError()
+                {
+                    Code = ErrorCode.VALIDATION,
+                    Message = ex.Message,
+                };
+                result.Errors.Add(error);
+            });
+
         }
         catch (Exception ex)
         {
