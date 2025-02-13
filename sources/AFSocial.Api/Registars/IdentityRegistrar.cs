e861ffc509da2975287bf2494355ab4db89e359a
host1812 <host1812@gmail.com>
2025-02-13 15:23:28 -0500

    add jwt settings in registar

---
+++ sources/AFSocial.Api/Registars/IdentityRegistrar.cs
@@ -0,0 +1,16 @@
+﻿
+using AFSocial.Application.Options;
+
+namespace AFSocial.Api.Registars;
+
+public class IdentityRegistrar : IWebApplicationBuilderRegistar
+{
+    public void RegisterServices(WebApplicationBuilder builder)
+    {
+        var jwtSettings = new JwtSettings();
+        builder.Configuration.Bind(nameof(JwtSettings), jwtSettings);
+        
+        var jwtSection = builder.Configuration.GetSection(nameof(JwtSettings));
+        builder.Services.Configure<JwtSettings>(jwtSection);
+    }
+}
