1f0383815adb46455cdd81bbe7241e9152121659
host1812 <host1812@gmail.com>
2025-02-13 16:14:37 -0500

    add jwt configuration in registrar

--- sources/AFSocial.Api/Registars/IdentityRegistrar.cs
+++ sources/AFSocial.Api/Registars/IdentityRegistrar.cs
@@ -1,5 +1,8 @@
 ﻿
 using AFSocial.Application.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace AFSocial.Api.Registars;
 
@@ -12,5 +15,28 @@ public class IdentityRegistrar : IWebApplicationBuilderRegistar
         
         var jwtSection = builder.Configuration.GetSection(nameof(JwtSettings));
         builder.Services.Configure<JwtSettings>(jwtSection);
+
+        builder.Services.AddAuthentication(a =>
+        {
+            a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            a.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            a.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(jwt =>
+        {
+            jwt.SaveToken = true;
+            jwt.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.SigningKey)),
+                ValidateIssuer = true,
+                ValidIssuer = jwtSettings.Issuer,
+                ValidateAudience = true,
+                ValidAudiences = jwtSettings.Audiences,
+                RequireExpirationTime = false,
+                ValidateLifetime = true,
+            };
+            jwt.Audience = jwtSettings.Audiences.FirstOrDefault();
+            jwt.ClaimsIssuer = jwtSettings.Issuer;
+        });
     }
 }
